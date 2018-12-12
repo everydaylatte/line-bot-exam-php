043c9999eb59c32e71390ff401e3a5754e6434c2
everydaylatte <43552517+everydaylatte@users.noreply.github.com>
2018-12-13 03:50:20 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Ou/PR4gQF8HODIhktVr42Cvy1a/HhvPVEMyzw+E6k0yHrk5rbtWBL4gZlpwLA30WgWkZSa9zQ3A6m1CCLN7MkXZy6QxoKQqGM+aY8SqKVUWCNhIt9xygxM2FnZq4iIPgnyrcDgSwzZEiTj+m/mcAEAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
