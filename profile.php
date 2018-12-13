951ec8fc96016645691511bd48ae9c9d4725fbb8
everydaylatte <43552517+everydaylatte@users.noreply.github.com>
2018-12-14 03:31:55 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = 'Ou/PR4gQF8HODIhktVr42Cvy1a/HhvPVEMyzw+E6k0yHrk5rbtWBL4gZlpwLA30WgWkZSa9zQ3A6m1CCLN7MkXZy6QxoKQqGM+aY8SqKVUWCNhIt9xygxM2FnZq4iIPgnyrcDgSwzZEiTj+m/mcAEAdB04t89/1O/w1cDnyilFU=';
+$access_token = '';
 
-$userId = 'Ue92a4b648ab700533c529a7dcb7d3317';
+$userId = '';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
